4d7c9911d2a76658cd6ecca20b4d3cb5f52e49ef
Eric12110 <yes850520004@gmail.com>
2025-03-25 10:33:54 +0800

    更改按鈕名稱

--- src/Composition.vue
+++ src/Composition.vue
@@ -8,7 +8,7 @@
   <input type="text" ref="inputRef" />
   <Component_v5 :name="name" @getName="getName" />
   {{ count }}
-  <button @click="increment">button3</button>
+  <button @click="increment">button4</button>
   </template>
   
   <script>              
